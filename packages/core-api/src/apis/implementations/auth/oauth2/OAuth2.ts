6db64660247237d64f1bee75a9b255e820e7cd09
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2020-08-12 09:59:25 +0200

    Extend oauth2 provider type and allow to pass custom scopes

--- packages/core-api/src/apis/implementations/auth/oauth2/OAuth2.ts
+++ packages/core-api/src/apis/implementations/auth/oauth2/OAuth2.ts
@@ -27,6 +27,7 @@ import {
 import {
   AuthRequestOptions,
   BackstageIdentity,
+  BackstageIdentityApi,
   OAuthApi,
   OpenIdConnectApi,
   ProfileInfo,
@@ -42,6 +43,8 @@ type CreateOptions = {
 
   environment?: string;
   provider?: AuthProvider & { id: string };
+
+  defaultScopes?: string[];
 };
 
 export type OAuth2Response = {
@@ -61,15 +64,19 @@ const DEFAULT_PROVIDER = {
   icon: OAuth2Icon,
 };
 
-const SCOPE_PREFIX = '';
-
 class OAuth2
-  implements OAuthApi, OpenIdConnectApi, ProfileInfoApi, SessionStateApi {
+  implements
+    OAuthApi,
+    OpenIdConnectApi,
+    ProfileInfoApi,
+    SessionStateApi,
+    BackstageIdentityApi {
   static create({
     discoveryApi,
     environment = 'development',
     provider = DEFAULT_PROVIDER,
     oauthRequestApi,
+    defaultScopes = [],
   }: CreateOptions) {
     const connector = new DefaultAuthConnector({
       discoveryApi,
@@ -93,11 +100,7 @@ class OAuth2
 
     const sessionManager = new RefreshingAuthSessionManager({
       connector,
-      defaultScopes: new Set([
-        'openid',
-        `${SCOPE_PREFIX}userinfo.email`,
-        `${SCOPE_PREFIX}userinfo.profile`,
-      ]),
+      defaultScopes: new Set(defaultScopes),
       sessionScopes: (session: OAuth2Session) => session.providerInfo.scopes,
       sessionShouldRefresh: (session: OAuth2Session) => {
         const expiresInSec =
